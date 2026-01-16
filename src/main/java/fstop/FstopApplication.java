5b04ee089973d9837ecdbde093d718bf7ea985e5
Tonny Santana <tonnysantanasantos@gmail.com>
2026-01-16 12:22:39 -0300

    first commit

---
+++ src/main/java/fstop/FstopApplication.java
@@ -0,0 +1,13 @@
+package fstop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class FstopApplication {
+    
+    void main(String[] args) {
+        run(FstopApplication.class, args);
+    }
+    
+}
