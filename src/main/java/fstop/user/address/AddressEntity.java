f4690f9eb2f011c1771800cdcd588856dc929c9a
Tonny Santana <tonnysantanasantos@gmail.com>
2026-01-19 09:45:15 -0300

    chore: create address domain

---
+++ src/main/java/fstop/user/address/AddressEntity.java
@@ -0,0 +1,39 @@
+package fstop.user.address;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import fstop.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+/**
+ * @author Tonny Santana
+ * @date 19/01/2026 09:20
+ * @since 1.0.0
+ */
+
+@Entity
+@Table(name = "addresses")
+@Data
+public class AddressEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private UserEntity user;
+    
+    private String street;
+    private String neighborhood;
+    private String city;
+    private String country;
+    private Short number;
+    
+    @Column(name = "address_complement")
+    private String addressComplement;
+    
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+}
